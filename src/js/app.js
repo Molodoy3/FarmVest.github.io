fc64ee8778f3149a52c59f476c085b2d0e742298
Molodoy3 <artem.nikonov.2005@bk.ru>
2023-02-26 21:30:07 +0500

    add main-block, brands and how-work

--- src/js/app.js
+++ src/js/app.js
@@ -7,11 +7,15 @@ functions.isWebp(); //запуск функции определения под�
 import {DynamicAdapt} from './modules/dynamic.js';
 import {delegationClick} from './modules/script.js';
 import {headerScroll} from './modules/script.js';
+import { animOnScroll } from './modules/animOnScroll.js';
+import { formValidate } from './modules/formValidate.js';
 
 window.addEventListener("load", windowLoad);
 function windowLoad() {
     document.body.classList.add('load');
     headerScroll();
+    animOnScroll();
     delegationClick();
     DynamicAdapt();
+    formValidate();
 }
